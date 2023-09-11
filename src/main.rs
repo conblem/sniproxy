0c414846975aa757e13c4fd003a85af02ed8e372
conblem <mail@conblem.me>
2023-09-11 22:19:08 +0200

    wip

--- src/main.rs
+++ src/main.rs
@@ -8,46 +8,69 @@ use tls_parser::{
 };
 use tokio::io::{copy_bidirectional, AsyncReadExt, AsyncWriteExt};
 use tokio::net::{TcpListener, TcpStream};
+use tokio::runtime::Runtime;
+use tracing::field::display;
+use tracing::{info, Span};
+use tracing_attributes::instrument;
+use tracing_futures::Instrument;
 use trust_dns_resolver::config::{ResolverConfig, ResolverOpts};
+use trust_dns_resolver::proto::rr::rdata::A;
 use trust_dns_resolver::TokioAsyncResolver;
+use url::Url;
 
 #[derive(Parser, Debug)]
 #[command(author, version, about, long_about = None)]
 struct Args {
     /// Name of the person to greet
     #[arg(long)]
-    socks: Option<String>,
+    socks: Option<Url>,
 
     #[arg(long, default_value = "443")]
     port: u16,
+
+    #[arg(long, default_value = "0.0.0.0")]
+    listen: String,
 }
 
-#[tokio::main]
-async fn main() -> Result<(), Box<dyn Error>> {
+fn main() -> Result<(), Box<dyn Error>> {
     let args = Args::parse();
-    let socks = args.socks.map(|socks| &*socks.leak());
+    let socks = args.socks.map(Arc::new);
 
-    let listener = TcpListener::bind(format!("127.0.0.1:{}", args.port)).await?;
+    let rt = Runtime::new()?;
+    tracing_subscriber::fmt::init();
+    rt.block_on(
+        async move {
+            let listener = TcpListener::bind(format!("{}:{}", args.listen, args.port)).await?;
 
-    let resolver =
-        TokioAsyncResolver::tokio(ResolverConfig::cloudflare_tls(), ResolverOpts::default());
-    let resolver = Arc::new(resolver);
+            let resolver = TokioAsyncResolver::tokio(
+                ResolverConfig::cloudflare_tls(),
+                ResolverOpts::default(),
+            );
+            let resolver = Arc::new(resolver);
 
-    loop {
-        let (socket, _) = listener.accept().await?;
-        let resolver = Arc::clone(&resolver);
-        tokio::spawn(async move {
-            process_socket(socket, resolver.as_ref(), socks)
-                .await
-                .unwrap();
-        });
-    }
+            loop {
+                let (socket, _) = listener.accept().await?;
+                let resolver = Arc::clone(&resolver);
+                let socks = socks.as_ref().map(Arc::clone);
+                tokio::spawn(
+                    async move {
+                        process_socket(socket, resolver.as_ref(), socks.as_deref())
+                            .await
+                            .unwrap();
+                    }
+                    .in_current_span(),
+                );
+            }
+        }
+        .in_current_span(),
+    )
 }
 
+#[instrument(skip(resolver), err)]
 async fn process_socket(
     mut stream: TcpStream,
     resolver: &TokioAsyncResolver,
-    socks: Option<&str>,
+    socks: Option<&Url>,
 ) -> Result<(), Box<dyn Error>> {
     let mut buffer = [0u8; 1024];
     let n = stream.read_buf(&mut buffer.as_mut()).await?;
@@ -59,7 +82,7 @@ async fn process_socket(
     println!("SNI: {}", sni);
 
     let mut upstream = match socks {
-        Some(socks) => upstream_socks(sni, socks).await?,
+        Some(socks) => upstream_socks(sni, socks, resolver).await?,
         None => upstream_direct(sni, resolver).await?,
     };
     upstream.write_all(buffer).await?;
@@ -70,24 +93,51 @@ async fn process_socket(
     Ok(())
 }
 
+#[instrument(skip(resolver), fields(record), err)]
+async fn lookup_ip(sni: &str, resolver: &TokioAsyncResolver) -> Result<A, Box<dyn Error>> {
+    let lookup = resolver.ipv4_lookup(sni).await?;
+
+    let record = lookup.iter().next().ok_or("No IP found")?;
+    info!("IP: {:?}", record);
+    Span::current().record("record", display(record));
+
+    Ok(*record)
+}
+
+#[instrument(skip(resolver), err)]
 async fn upstream_direct(
     sni: &str,
     resolver: &TokioAsyncResolver,
 ) -> Result<TcpStream, Box<dyn Error>> {
-    let lookup = resolver.ipv4_lookup(sni).await?;
+    let target = lookup_ip(sni, resolver).await?;
 
-    let record = lookup.iter().next().ok_or("No IP found")?;
-    let addr = format!("{}:443", record);
-
-    Ok(TcpStream::connect(addr).await?)
+    Ok(TcpStream::connect(format!("{}:443", target)).await?)
 }
 
-async fn upstream_socks(sni: &str, socks: &str) -> Result<TcpStream, Box<dyn Error>> {
-    let upstream = Socks5Stream::connect(socks, sni.to_string(), 443, Config::default()).await?;
+#[instrument(skip(resolver), err)]
+async fn upstream_socks(
+    sni: &str,
+    socks: &Url,
+    resolver: &TokioAsyncResolver,
+) -> Result<TcpStream, Box<dyn Error>> {
+    let target = match socks.scheme() {
+        "socks5" => lookup_ip(sni, resolver).await?.to_string(),
+        "socks5h" => sni.to_string(),
+        _ => Err("Invalid scheme for socks")?,
+    };
+
+    let port = socks.port().unwrap_or(1080);
+    let socks = match socks.host_str() {
+        Some(host) => format!("{}:{}", host, port),
+        None => Err("Invalid socks url")?,
+    };
+
+    let upstream = Socks5Stream::connect(socks.as_str(), target, 443, Config::default()).await?;
 
     Ok(upstream.get_socket())
 }
 
+#[instrument(skip_all, err)]
 fn parse_extensions(buffer: &[u8]) -> Result<Vec<TlsExtension>, Box<dyn Error>> {
     let msg = match parse_tls_plaintext(buffer) {
         Ok((_, plaintext)) => plaintext.msg,
@@ -113,6 +163,7 @@ fn parse_extensions(buffer: &[u8]) -> Result<Vec<TlsExtension>, Box<dyn Error>> 
     }
 }
 
+#[instrument(skip_all, err)]
 fn parse_sni(extensions: Vec<TlsExtension>) -> Result<&str, Box<dyn Error>> {
     let sni = extensions
         .iter()
