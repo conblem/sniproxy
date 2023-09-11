dd8ade4dbc01a78451b5c02b6253cb5c6072e862
conblem <mail@conblem.me>
2023-09-11 18:53:27 +0200

    add basic command line parsing

--- src/main.rs
+++ src/main.rs
@@ -1,89 +1,130 @@
-use std::error::Error;
-use std::net::SocketAddr;
-use tls_parser::{gen_tls_clienthello, gen_tls_extensions, parse_tls_client_hello_extension, parse_tls_extensions, parse_tls_plaintext, Serialize, TlsExtension, TlsMessage, TlsMessageHandshake};
-use tokio::io::{AsyncReadExt, AsyncWriteExt, copy_bidirectional};
-use tokio::net::{lookup_host, TcpListener, TcpStream};
-use cookie_factory::{gen_simple};
+use clap::Parser;
 use fast_socks5::client::{Config, Socks5Stream};
+use std::error::Error;
+use std::sync::Arc;
+use tls_parser::{
+    parse_tls_client_hello_extensions, parse_tls_plaintext, TlsExtension, TlsMessage,
+    TlsMessageHandshake,
+};
+use tokio::io::{copy_bidirectional, AsyncReadExt, AsyncWriteExt};
+use tokio::net::{TcpListener, TcpStream};
+use trust_dns_resolver::config::{ResolverConfig, ResolverOpts};
+use trust_dns_resolver::TokioAsyncResolver;
+
+#[derive(Parser, Debug)]
+#[command(author, version, about, long_about = None)]
+struct Args {
+    /// Name of the person to greet
+    #[arg(long)]
+    socks: Option<String>,
+
+    #[arg(long, default_value = "443")]
+    port: u16,
+}
 
 #[tokio::main]
 async fn main() -> Result<(), Box<dyn Error>> {
-    let listener = TcpListener::bind("127.0.0.1:9443").await?;
+    let args = Args::parse();
+    let socks = args.socks.map(|socks| &*socks.leak());
+
+    let listener = TcpListener::bind(format!("127.0.0.1:{}", args.port)).await?;
+
+    let resolver =
+        TokioAsyncResolver::tokio(ResolverConfig::cloudflare_tls(), ResolverOpts::default());
+    let resolver = Arc::new(resolver);
 
     loop {
         let (socket, _) = listener.accept().await?;
+        let resolver = Arc::clone(&resolver);
         tokio::spawn(async move {
-            process_socket(socket).await.unwrap();
+            process_socket(socket, resolver.as_ref(), socks)
+                .await
+                .unwrap();
         });
     }
 }
 
-async fn process_socket(mut stream: TcpStream) -> Result<(), Box<dyn Error>> {
+async fn process_socket(
+    mut stream: TcpStream,
+    resolver: &TokioAsyncResolver,
+    socks: Option<&str>,
+) -> Result<(), Box<dyn Error>> {
     let mut buffer = [0u8; 1024];
     let n = stream.read_buf(&mut buffer.as_mut()).await?;
-    let (rem, mut plaintext) = parse_tls_plaintext(&buffer[..n]).unwrap();
-    let msg = match plaintext.msg.first_mut() {
-        Some(msg) => msg,
-        None => return Err("No TLS message found".into()),
+    let buffer = &buffer[..n];
+
+    let extensions = parse_extensions(buffer)?;
+    let sni = parse_sni(extensions)?;
+
+    println!("SNI: {}", sni);
+
+    let mut upstream = match socks {
+        Some(socks) => upstream_socks(sni, socks).await?,
+        None => upstream_direct(sni, resolver).await?,
     };
-    let handshake = match msg {
-        TlsMessage::Handshake(handshake) => handshake,
-        _ => return Err("No TLS handshake found".into()),
+    upstream.write_all(buffer).await?;
+
+    let res = copy_bidirectional(&mut stream, &mut upstream).await;
+    println!("finished: {:?}", res);
+
+    Ok(())
+}
+
+async fn upstream_direct(
+    sni: &str,
+    resolver: &TokioAsyncResolver,
+) -> Result<TcpStream, Box<dyn Error>> {
+    let lookup = resolver.ipv4_lookup(sni).await?;
+
+    let record = lookup.iter().next().ok_or("No IP found")?;
+    let addr = format!("{}:443", record);
+
+    Ok(TcpStream::connect(addr).await?)
+}
+
+async fn upstream_socks(sni: &str, socks: &str) -> Result<TcpStream, Box<dyn Error>> {
+    let upstream = Socks5Stream::connect(socks, sni.to_string(), 443, Config::default()).await?;
+
+    Ok(upstream.get_socket())
+}
+
+fn parse_extensions(buffer: &[u8]) -> Result<Vec<TlsExtension>, Box<dyn Error>> {
+    let msg = match parse_tls_plaintext(buffer) {
+        Ok((_, plaintext)) => plaintext.msg,
+        Err(_) => Err("Failed to parse TLS plaintext")?,
+    };
+
+    let handshake = match msg.first() {
+        Some(TlsMessage::Handshake(handshake)) => handshake,
+        _ => Err("No TLS handshake found")?,
     };
     let client_hello = match handshake {
         TlsMessageHandshake::ClientHello(client_hello) => client_hello,
-        _ => return Err("No ClientHello found".into()),
+        _ => Err("No ClientHello found")?,
     };
     let extensions = match client_hello.ext {
         Some(extensions) => extensions,
-        None => return Err("No extensions found".into()),
+        None => Err("No extensions found")?,
     };
 
-
-    let mut new_extensions = Vec::new();
-    let mut buf = extensions;
-    loop {
-        let (reste, first) = parse_tls_client_hello_extension(buf).unwrap();
-        if matches!(first, TlsExtension::ALPN(_)) {
-            let end_of_stuff_before = extensions.len() - buf.len();
-            new_extensions.extend_from_slice(&extensions[..end_of_stuff_before]);
-            let test = extensions.len() - reste.len();
-            //new_extensions.resize(test, 0);
-            //new_extensions.extend_from_slice(&extensions[end_of_stuff_before..test]);
-            new_extensions.extend_from_slice(reste);
-            break;
-        }
-        buf = reste;
+    match parse_tls_client_hello_extensions(extensions) {
+        Ok((_, extensions)) => Ok(extensions),
+        Err(_) => Err("Failed to parse TLS extensions")?,
     }
+}
 
-
-    let (_, mut extensions) = parse_tls_extensions(extensions).unwrap();
+fn parse_sni(extensions: Vec<TlsExtension>) -> Result<&str, Box<dyn Error>> {
     let sni = extensions
         .iter()
         .filter_map(|ext| match ext {
             TlsExtension::SNI(sni) => Some(sni),
             _ => None,
         })
-        .next();
+        .next()
+        .ok_or("SNI Extension not found")?;
 
-    let (_, sni) = match sni.and_then(|sni| sni.iter().next()) {
-        Some(sni) => sni,
-        None => return Err("No SNI found".into()),
-    };
-
-    let sni = std::str::from_utf8(sni)?;
-
-    println!("SNI: {:?}", sni);
-
-    client_hello.ext = Some(&new_extensions);
-
-    let mut upstream = TcpStream::connect("76.76.21.21:443").await?;
-
-    let mut msg = plaintext.serialize()?;
-    upstream.write_all(&msg).await?;
-
-    let res = copy_bidirectional(&mut stream, &mut upstream).await;
-    println!("finished: {:?}", res);
-
-    Ok(())
+    match sni.first() {
+        Some((_, sni)) => Ok(std::str::from_utf8(sni)?),
+        None => Err("Couldn't parse SNI Name")?,
+    }
 }
