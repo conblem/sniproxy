18664e455a577a4f3ba26cc3e3c204c52ae3cc90
conblem <mail@conblem.me>
2023-10-03 16:48:02 +0200

    improve build

--- build.sh
+++
@@ -1,5 +0,0 @@
-#!/usr/bin/env bash
-
-cross build --target aarch64-unknown-linux-musl --release
-cp target/aarch64-unknown-linux-musl/release/sniproxy arm64
-docker build . -t conblem/sniproxy:latest --platform=linux/arm64
